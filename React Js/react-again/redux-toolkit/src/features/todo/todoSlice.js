390fcd7e2896ef02e98f31903ccf6e5863540dc5
Dhvanit <monparadhvanit@gmail.com>
2024-07-02 15:50:18 +0530

    react complete

--- React Js/react-again/redux-toolkit/src/features/todo/todoSlice.js
+++ React Js/react-again/redux-toolkit/src/features/todo/todoSlice.js
@@ -1,36 +1,42 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 export const todoSlice = createSlice({
-    name: 'todo',
-    initialState: {
-        todos:
-            JSON.parse(localStorage.getItem("TodoLists")) !== null
-                ? JSON.parse(localStorage.getItem("TodoLists"))
-                : [],
-        theme: false,
-        edit: false,
-        editVal: '',
-        editInfo: "",
+  name: 'todo',
+  initialState: {
+    todos:
+      JSON.parse(localStorage.getItem("TodoLists")) !== null
+        ? JSON.parse(localStorage.getItem("TodoLists"))
+        : [],
+    editingValue: {},
+    editingStatus: false
+  },
+  reducers: {
+    addTodo: (state, action) => {
+      if (action.payload) {
+        const todo = { id: nanoid(), text: action.payload };
+        state.todos.push(todo);
+      }
     },
-    reducers: {
-        addTodo: (state, action) => {
-          const todo = { id: nanoid(), text: action.payload };
-          state.todos.push(todo);
-        },
-        removeTodo: (state, action) => {
-          state.todos = state.todos.filter((todo) => todo.id !== action.payload);
-        },
-        updateTodo: (state, action) => {
-          state.todos = state.todos.map((todo) =>
-            todo.id === action.payload.id
-              ? { ...todo, text: action.payload.text }
-              : todo
-          );
-        },
-      },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      state.todos = state.todos.map((todo) =>
+        todo.id === action.payload.id
+          ? { ...todo, text: action.payload.text }
+          : todo
+      );
+    },
+    setEditingValue: (state, action) => {
+      state.editingValue = action.payload
+    },
+    setEditingStatus: (state, action) => {
+      state.editingStatus = action.payload
+    }
+  },
 })
 
-export const { addTodo, removeTodo, updateTodo } = todoSlice.actions
+export const { addTodo, removeTodo, updateTodo, setEditingStatus, setEditingValue } = todoSlice.actions
 
 export default todoSlice.reducer
 
