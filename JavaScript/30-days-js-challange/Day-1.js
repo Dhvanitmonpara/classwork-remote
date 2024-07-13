b78697b539a12606f8f7f177891f747ea99ba88f
Dhvanit <monparadhvanit@gmail.com>
2024-07-13 12:10:53 +0530

    started 30 days js challange

---
+++ JavaScript/30-days-js-challange/Day-1.js
@@ -0,0 +1,36 @@
+// var declaration
+
+var a1 = 10
+console.log(a1)
+
+let a2 = "Hello, Kaise ho"
+console.log(a2)
+
+const a3 = true
+console.log(a3)
+
+// data types
+
+let b1 = 13
+let b2 = "This is string"
+let b3 = true
+let b4 = {
+    isThisAnObject: true,
+    whatAboutString: "A typical string msg"
+}
+let b5 = ["1", 2, "three"]
+
+// reassign variable
+
+let c1 = 10
+console.log("Before reassignment: ", c1)
+c1 = "Hey"
+console.log("After reassignment: ", c1)
+
+// reassignment using const
+
+const d1 = 40
+console.log("Before reassignment: ", d1)
+// d1 = "Error" // This will throw an error
+console.log("After reassignment: ", d1)
+
