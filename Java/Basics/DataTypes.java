f4abf08f49bf61503b05a4c028f69b8b22d03279
Dhvanit <monparadhvanit@gmail.com>
2024-07-22 18:06:05 +0530

    Day-8

---
+++ Java/Basics/DataTypes.java
@@ -0,0 +1,22 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        
+        // primitive data types
+
+        int a = 10; // 4 bytes
+        System.out.println("The value of a is " + a + "which is a integer");
+
+        double b = 3.14; // 8 bytes/
+        System.out.println("The value of b is " + b + "which is a double");
+
+        boolean c = true; // 1 bit
+        System.out.println("The value of c is " + c + "which is a boolean");
+
+        float d = 3.14f;
+        System.out.println("The value of d is " + d + "which is a float");
+
+        char e = 'A';
+        System.out.println("The value of e is " + e + "which is a char");
+
+    }
+}
